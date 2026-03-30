bef87050373bc22a0e44bbf11b0e6eaef605b717
김민정 <gnujnim475@gmail.com>
2026-03-30 16:45:10 +0900

    chore : 프로젝트 초기 세팅

---
+++ src/main/java/com/project/sentic/SenticApplication.java
@@ -0,0 +1,13 @@
+package com.project.sentic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/sentic/SenticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sentic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
