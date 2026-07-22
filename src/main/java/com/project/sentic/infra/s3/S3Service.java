20ff4f8f9fe11a0d15228c2866efca6cf81e270a
min-xeox <73517404+min-xeox@users.noreply.github.com>
2026-07-23 03:06:43 +0900

    fix: s3 업로드 URL을 Presigned URL 방식으로 변경 (#23)

--- src/main/java/com/project/sentic/infra/s3/S3Service.java
+++ src/main/java/com/project/sentic/infra/s3/S3Service.java
@@ -1,6 +1,8 @@
 package com.project.sentic.infra.s3;
 
+import com.amazonaws.HttpMethod;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.project.sentic.global.exception.CustomException;
 import com.project.sentic.global.exception.ErrorCode;
@@ -10,6 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayInputStream;
+import java.util.Date;
 import java.util.UUID;
 
 @Slf4j
@@ -33,7 +36,15 @@ public class S3Service {
         try {
             amazonS3.putObject(bucket, fileName, new ByteArrayInputStream(audioData), metadata);
             log.info("[S3] 업로드 완료: {}", fileName);
-            return amazonS3.getUrl(bucket, fileName).toString();
+
+            // Presigned URL 생성 (1시간 유효)
+            Date expiration = new Date(System.currentTimeMillis() + 1000 * 60 * 60);
+            GeneratePresignedUrlRequest presignedUrlRequest =
+                    new GeneratePresignedUrlRequest(bucket, fileName)
+                            .withMethod(HttpMethod.GET)
+                            .withExpiration(expiration);
+
+            return amazonS3.generatePresignedUrl(presignedUrlRequest).toString();
         } catch (Exception e) {
             log.error("[S3] 업로드 실패: {}", e.getMessage());
             throw new CustomException(ErrorCode.INTERNAL_SERVER_ERROR);
