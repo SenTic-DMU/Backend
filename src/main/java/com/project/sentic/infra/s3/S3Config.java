0fbd920ff365e0d1413442e8a5acca22f3ee5cb7
gnujnim475 <gnujnim475@gmail.com>
2026-09-25 23:59:05 +0900

    feat: AWS 운영 배포 설정 및 S3 연동 개선 (#47)
    * chore: node_modules 추적 제거 및 gitignore 정리
    
    * feat: AWS 운영 배포 설정 및 S3 SDK v2 전환
    
    * fix: AI 테스트 API 인증 적용

--- src/main/java/com/project/sentic/infra/s3/S3Config.java
+++ src/main/java/com/project/sentic/infra/s3/S3Config.java
@@ -1,36 +1,32 @@
 package com.project.sentic.infra.s3;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicSessionCredentials;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class S3Config {
-
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
-
-    @Value("${cloud.aws.credentials.session-token}")
-    private String sessionToken;
 
     @Value("${cloud.aws.region.static}")
     private String region;
 
     @Bean
-    public AmazonS3 amazonS3() {
-        BasicSessionCredentials credentials = new BasicSessionCredentials(
-                accessKey, secretKey, sessionToken
-        );
-        return AmazonS3ClientBuilder.standard()
-                .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .region(Region.of(region))
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
